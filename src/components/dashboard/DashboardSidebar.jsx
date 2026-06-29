7c34105f8da23b657895101fe189aa2fc58a25db
JerinSikhder7580 <jerinahmed615@gmail.com>
2026-06-29 22:09:06 +0600

    Dashboard sidebar

--- src/components/dashboard/DashboardSidebar.jsx
+++ src/components/dashboard/DashboardSidebar.jsx
@@ -53,14 +53,7 @@ export default async function DashboardSidebar() {
     }
     const navItems = dashboardItems[role]
 
-    // const navItems = [
-    //     { icon: House, label: "Home" },
-    //     { icon: Magnifier, label: "Search" },
-    //     { icon: Bell, label: "Notifications" },
-    //     { icon: Envelope, label: "Messages" },
-    //     { icon: Person, label: "Profile" },
-    //     { icon: Gear, label: "Settings" },
-    // ];
+   
 
     return (
         <div className="p-6 border" >
@@ -115,15 +108,19 @@ export default async function DashboardSidebar() {
                 </nav>
 
                 <Drawer.Backdrop>
+
                     <Drawer.Content placement="left">
                         <Drawer.Dialog>
                             <Drawer.CloseTrigger />
+
                             <Drawer.Header>
                                 <Drawer.Heading>Navigation</Drawer.Heading>
                             </Drawer.Header>
                             <Drawer.Body>
+
                                 <nav className="flex flex-col gap-1">
                                     {navItems?.map((item) => (
+                                        
                                         <button
                                             key={item.label}
                                             className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
@@ -136,9 +133,16 @@ export default async function DashboardSidebar() {
                                     ))}
                                 </nav>
                             </Drawer.Body>
+
+
                         </Drawer.Dialog>
                     </Drawer.Content>
+
+
                 </Drawer.Backdrop>
+
+
+
             </Drawer>
         </div>
     );
